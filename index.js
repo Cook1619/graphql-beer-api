c7400ef43e94a5e0a96bd6686c6bf1024050a271
Cook1619 <cook6348@gmail.com>
2020-04-13 18:51:39 -0500

    Added port

--- index.js
+++ index.js
@@ -1,72 +1,75 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
 //Schema, is the the shape of the data, which is why is works with typescript so well
 //Defines all of the types of the data
 
 const typeDefs = gql`
+  enum Status {
+    LIKED
+    NOT_LIKED
+    INDIFFERENT
+  }
 
-    enum Status {
-        LIKED
-        NOT_LIKED
-        INDIFFERENT
-    }
+  type Grains {
+    id: ID
+    name: String
+  }
 
-    type Grains {
-        id: ID
-        name: String
-    }
+  type Beer {
+    id: ID!
+    name: String!
+    color: String!
+    abv: Float!
+    rating: Int
+    grains: [Grains]
+    status: Status
+  }
 
-    type Beer {
-        id: ID!
-        name: String!
-        color: String!
-        abv: Float!
-        rating: Int
-        grains: [Grains]
-        status: Status
-    }
-
-    type Query {
-        beers: [Beer]
-        beer(id: ID): Beer
-    }
-
-`
+  type Query {
+    beers: [Beer]
+    beer(id: ID): Beer
+  }
+`;
 //This is just dummy data
 const beers = [
-    {
-        id: '123',
-        name: 'Castle Danger Creame Ale',
-        color: 'Light Amber',
-        abv: '5.5',
-        rating: 10,
-        grains: [{ id: 1, name: 'Malt'}, { id: 2, name: 'Barley'}]
-    },
-    {
-        id: '456',
-        name: 'Waconia Brewing Amber Ale',
-        color: 'Amber',
-        abv: '6.0',
-        rating: 9
-    }
-]
+  {
+    id: "123",
+    name: "Castle Danger Creame Ale",
+    color: "Light Amber",
+    abv: "5.5",
+    rating: 10,
+    grains: [
+      { id: 1, name: "Malt" },
+      { id: 2, name: "Barley" },
+    ],
+  },
+  {
+    id: "456",
+    name: "Waconia Brewing Amber Ale",
+    color: "Amber",
+    abv: "6.0",
+    rating: 9,
+  },
+];
 
 const resolvers = {
-    Query: {
-        beers: () => {
-            return beers;
-        },
-        beer: (obj, { id }, context, info) => {
-            const foundBeer = beers.find((beer) => {
-                return beer.id === id
-            })
-            return foundBeer;
-        }
-    }
-}
+  Query: {
+    beers: () => {
+      return beers;
+    },
+    beer: (obj, { id }, context, info) => {
+      const foundBeer = beers.find((beer) => {
+        return beer.id === id;
+      });
+      return foundBeer;
+    },
+  },
+};
 
-const server =  new ApolloServer({ typeDefs, resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-    console.log(`Server is started at ${url}`)
+server.listen({
+  port: process.env.port || 4000  
+}).then(({ url }) => {
+  console.log(`Server is started at ${url}`);
 });
